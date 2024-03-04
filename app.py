b1b169d8a3586dc8bc4a3a16fdec239cd439b0ce
Rahul Manocha <104346573+RahulManocha21@users.noreply.github.com>
2024-03-05 00:17:01 +0530

    app.py

--- app.py
+++ app.py
@@ -14,29 +14,7 @@ def get_gemini_response(input, image):
 st.set_page_config(page_title="🪴 Plant Identification")
 st.header("Plant Identification ☘️")
 st.sidebar.header('Fill the Details to get the Customized Response')
-countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia",
-    "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
-    "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-    "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia",
-    "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czechia", "Denmark", "Djibouti", "Dominica",
-    "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia",
-    "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece",
-    "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India",
-    "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan",
-    "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos",
-    "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar",
-    "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico",
-    "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
-    "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman",
-    "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar",
-    "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa",
-    "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone",
-    "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain",
-    "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand",
-    "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine",
-    "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
-    "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]
-Location = st.sidebar.selectbox('Location', countries)
+Location = st.sidebar.selectbox('Location', ["United States"])
 Gardening_Experience  = st.sidebar.selectbox('How much experience do you have with gardening?',['Beginner', 'Intermediate','Advanced'])
 PlantPrefrence = st.sidebar.multiselect("Select Plants:", ["🌸 Flowers","🌿 Herbs","🥦 Vegetables","🍓 Fruits","🌳 Trees","🌴 Shrubs","🌾 Grasses","🌵 Succulents","🌵 Cacti","🌿 Ferns","🌱 Mosses","🌿 Vines","💧 Aquatics","🌷 Bulbs","🌺 Orchids"])   
 Style  = st.sidebar.selectbox("🌿 Gardening Style", ('Organic', 'Conventional'))
@@ -54,7 +32,7 @@ if option == "Upload Image":
     uploaded_file = st.file_uploader("Choose an 🪴 image...", type=["jpg", "jpeg", "png"])
 elif option =='Capture from Camera':
     uploaded_file = st.camera_input('Camera Access', label_visibility="visible")
-    
+
 # Display the uploaded image
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -65,7 +43,7 @@ submit = st.button("Get Response")
 
 # If the button is clicked
 if submit:
-    if input_question and image:
+    if image is not None and input_question is not None:
         input_text = f'''You are a botanical expert, study the image and respond accordingly. 
         If there is no plant in the image, respond as No plant found. else
         I am from {Location}, I am at {Gardening_Experience} level in gardening, I want to have {PlantPrefrence} in my garden.
@@ -79,8 +57,7 @@ if submit:
         # Display the response
         st.subheader("Response : ")
         st.write(response)
-
-    elif input_question:
+    elif input_question is not None:
         input_text = f'''You are a botanical expert,
         I am from {Location}, I am at {Gardening_Experience} level in gardening, I want to have {PlantPrefrence} in my garden.
         I want to do in {Style} style gardening.  My budget is {Budget}, time is {Time} and Maintenance preference is {Maintenance}. 
@@ -93,8 +70,7 @@ if submit:
         # Display the response
         st.subheader("Response : ")
         st.write(response)
-
-    elif image:
+    elif image is not None:
         input_text = f'''You are a botanical expert, study the image and respond accordingly. 
         If there is no plant in the image, respond as No plant found. else
         I am from {Location}, I am at {Gardening_Experience} level in gardening, I want to have {PlantPrefrence} in my garden.
